c68292f35d4c9c03b46c949769ea6a38a2a94858
996wuxian <1640551913@qq.com>
2024-10-15 16:21:44 +0800

    feat-数据库连接

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'wuxian',
+      database: 'template-nest-db',
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      retryDelay: 500,
+      retryAttempts: 10,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
