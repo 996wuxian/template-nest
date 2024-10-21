85d2846c14535f2c24d22f71dc2ed12bb5a2ee86
996wuxian <1640551913@qq.com>
2024-10-21 14:49:17 +0800

    feat(article): 文章模块新增查看和阅读量增长接口

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ArticleService } from './article.service';
-import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { ArticleService } from './article.service'
+import { CreateArticleDto } from './dto/create-article.dto'
+import { UpdateArticleDto } from './dto/update-article.dto'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 
-@Controller('article')
+@Controller('api/article')
+@ApiTags('文章')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Post()
   create(@Body() createArticleDto: CreateArticleDto) {
-    return this.articleService.create(createArticleDto);
+    return this.articleService.create(createArticleDto)
   }
 
   @Get()
   findAll() {
-    return this.articleService.findAll();
+    return this.articleService.findAll()
   }
 
   @Get(':id')
+  @ApiOperation({ summary: '根据id查询文章' })
   findOne(@Param('id') id: string) {
-    return this.articleService.findOne(+id);
+    return this.articleService.findOne(+id)
+  }
+
+  @Get(':id/view')
+  @ApiOperation({ summary: '阅读量, 请求递增' })
+  async view(@Param('id') id: string) {
+    return await this.articleService.view(+id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
-    return this.articleService.update(+id, updateArticleDto);
+    return this.articleService.update(+id, updateArticleDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.articleService.remove(+id);
+    return this.articleService.remove(+id)
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateArticleDto } from './dto/create-article.dto'
+import { UpdateArticleDto } from './dto/update-article.dto'
+import { Article } from './entities/article.entity'
+
+import { InjectEntityManager } from '@nestjs/typeorm'
+import { EntityManager } from 'typeorm'
 
 @Injectable()
 export class ArticleService {
+  @InjectEntityManager()
+  entityManager: EntityManager
   create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+    return 'This action adds a new article'
   }
 
   findAll() {
-    return `This action returns all article`;
+    return `This action returns all article`
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} article`;
+  async findOne(id: number) {
+    return await this.entityManager.findOneBy(Article, {
+      id
+    })
+  }
+
+  async view(id: number) {
+    const article = await this.findOne(id)
+
+    article.viewCount++
+
+    await this.entityManager.save(article)
+
+    return article.viewCount
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+    return `This action updates a #${id} article`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} article`;
+    return `This action removes a #${id} article`
   }
 }
